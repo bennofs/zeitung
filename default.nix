e1a5cefc8bc89f00ff966e7262bb8c9a716870a6
Benno Fünfstück <benno.fuenfstueck@gmail.com>
2022-02-26 15:25:52 +0100

    add flake

---
+++ default.nix
@@ -0,0 +1,26 @@
+{ stdenv, lib, makeWrapper, python3, geckodriver, rmapi, rclone }:
+
+let
+  pyenv = python3.withPackages (p: [geckodriver p.selenium p.requests]);
+in stdenv.mkDerivation {
+  name = "zeitung-0.1";
+  src = ./.;
+  buildInputs = [ pyenv makeWrapper geckodriver rmapi rclone ];
+  installPhase = ''
+    mkdir -p $out/bin
+
+    cp "$src"/*.py "$out/bin"
+    cp "$src"/*.sh "$out/bin"
+
+    patchShebangs "$out/bin"
+    for script in "$out/bin/"*.py; do
+      wrapProgram "$script" \
+        --prefix "PATH" : "${lib.makeBinPath [geckodriver]}"
+    done
+
+    for script in "$out/bin/"*.sh; do
+      wrapProgram "$script" \
+        --prefix "PATH" : "$out/bin:${lib.makeBinPath [rmapi rclone]}"
+    done
+  '';
+}
